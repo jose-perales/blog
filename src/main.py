b8ab4aa25f542db73d7cc7bb1e9ce09fad12a110
E. Jose Perales <ejoseperales@hey.com>
2024-08-23 15:42:01 -0400

    wip

--- src/main.py
+++ src/main.py
@@ -1,27 +1,17 @@
 from fasthtml.common import *
-from fasthtml.components import Zero_md
-from functools import partial
+
+hdrs = (MarkdownJS(), HighlightJS(langs=['python', 'javascript', 'html', 'css']), )
+
+app, rt = fast_app(hdrs=hdrs)
 
 with open('src/posts/first.md') as f: 
-    lesson_content = f.read()
-
-def render_local_md(md, css = ''):
-    css_template = Template(Style(css), data_append=True)
-    return Zero_md(css_template, Script(md, type="text/markdown"))
-
-lesson_content_html = render_local_md(lesson_content)
-
-zeromd_headers = [Script(type="module", src="https://cdn.jsdelivr.net/npm/zero-md@3?register")]
-
-app = FastHTML(hdrs=(zeromd_headers))
-
-rt = app.route
+    post = f.read()
 
 def Home():
-    return Div(lesson_content_html, cls="col-xs-5"),
-
+    return Titled("Exploratory Data Analysis", Div(post, cls="marked"))
 
 @rt('/')
-def get(): return Home()
+def get(req):
+    return Home()
 
 serve()
