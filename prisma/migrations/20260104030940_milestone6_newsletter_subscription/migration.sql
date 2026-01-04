258ff722bcffde2dc67ae7f73fbc3560cac8ec2b
Jose Perales <ejoseperales@gmail.com>
2026-01-04 03:12:31 +0000

    feat(db): add newsletter subscriptions

---
+++ prisma/migrations/20260104030940_milestone6_newsletter_subscription/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "NewsletterSubscription" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" TEXT,
+
+    CONSTRAINT "NewsletterSubscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "NewsletterSubscription_email_key" ON "NewsletterSubscription"("email");
